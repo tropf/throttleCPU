18be691a5fef624abf26030b85a3bd164aed106b
kali pso <kalipso@c3d2.de>
2018-04-05 22:55:58 +0000

    Inital Commit - Setted up CMake and a simple hello world to be able to generate ymc config.

---
+++ throttleCPU.cpp
@@ -0,0 +1,11 @@
+#include <chrono>
+#include <thread>
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    /* std::this_thread::sleep_for(std::chrono::milliseconds(x)); */
+    std::cout << "HELLO WOLRD" << std::endl;
+    return 0;
+}
+
